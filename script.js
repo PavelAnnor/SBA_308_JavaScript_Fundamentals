63471ed73e75052fc5c2d66b47f407d80777868c
PavelAnnor <annorpavel@gmail.com>
2026-05-18 16:22:30 -0400

    Created, get methods to get unique ids, scores for assignments

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
-// The provided course information.
+/*
+COURSE INFO OBJECT
+{
+  id: number,
+  name: "string",
+}
+  */
 const CourseInfo = {
   id: 451,
   name: "Introduction to JavaScript"
 };
 
-// The provided assignment group.
+
+
+/*
+ASSIGNMENT GROUP OBJECT (Assigment Groups for a particular course)
+{
+   id: number,
+   name: "string",
+  course_id: number,    // the ID of the course the assignment group belongs to
+  group_weight: number,  // the percentage weight of the entire assignment group
+  assignments: [AssignmentInfo],
+}
+ */
 const AssignmentGroup = {
   id: 12345,
   name: "Fundamentals of JavaScript",
   course_id: 451,
   group_weight: 25,
   assignments: [
+
+    /*
+    ASSIGMENT OBJECT (Assingment in an AssigmentGroup)
+    {
+            id: number,
+            name: string,
+            due_at: Date string,  // the due date for the assignment
+            points_possible: number,  // the maximum points possible for the assignment
+
+    } */
     {
       id: 1,
       name: "Declare a Variable",
       due_at: "2023-01-25",
       points_possible: 50
     },
+
     {
       id: 2,
       name: "Write a Function",
       due_at: "2023-02-27",
       points_possible: 150
     },
+
     {
       id: 3,
       name: "Code the World",
@@ -32,8 +61,24 @@ const AssignmentGroup = {
   ]
 };
 
-// The provided learner submission data.
+//An array of LearnerSubmission objects
 const LearnerSubmissions = [
+
+/*
+LEARNER SUBMISSION OBJECT (When the learner submits assigments)
+{
+
+    learner_id: number, 
+    assignment_id: number, //connects to assignment object
+    submission: {
+      submitted_at: Date string,
+      score: number
+    }
+
+}
+    
+    
+    */
   {
     learner_id: 125,
     assignment_id: 1,
@@ -42,6 +87,7 @@ const LearnerSubmissions = [
       score: 47
     }
   },
+
   {
     learner_id: 125,
     assignment_id: 2,
@@ -50,6 +96,7 @@ const LearnerSubmissions = [
       score: 150
     }
   },
+
   {
     learner_id: 125,
     assignment_id: 3,
@@ -58,6 +105,7 @@ const LearnerSubmissions = [
       score: 400
     }
   },
+
   {
     learner_id: 132,
     assignment_id: 1,
@@ -66,6 +114,7 @@ const LearnerSubmissions = [
       score: 39
     }
   },
+
   {
     learner_id: 132,
     assignment_id: 2,
@@ -73,29 +122,119 @@ const LearnerSubmissions = [
       submitted_at: "2023-03-07",
       score: 140
     }
-  }
+  },
+
+
 ];
 
-function getLearnerData(course, ag, submissions) {
-  // here, we would process this data to achieve the desired result.
-  const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0 // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833 // late: (140 - 15) / 150
-    }
-  ];
 
-  return result;
+//Helper Functions 
+//returns an array with unique learner ids
+function getUniqueLearnersIDs(submissionsArr){
+    let x = [];
+    for(submission of submissionsArr){
+        if(x.indexOf(submission.learner_id)===-1)
+            x.push(submission.learner_id)
+    }
+    return x
 }
+
+
+//Returns what a student scored on a particular assignment. Paramaters: student_id, assignment_id, and submissions Array
+function getAssignmentScore(student, assigmentNumber, submissions){
+    let score = 0;
+    let ptsPossible = 0;
+    //gets studentScore on a particual assigment 
+    for(let submitted of submissions){
+        if(submitted.learner_id===student && submitted.assignment_id===assigmentNumber){
+            score=  submitted.submission.score
+        }
+    }
+
+    // get pts possible from that assignment as well 
+    for(let assigment of AssignmentGroup.assignments){
+        if (assigmentNumber===assigment.id)
+            ptsPossible =assigment.points_possible
+    }
+
+    return score/ptsPossible
+
+}
+
+//returns an array of assignemtns a student completed. Paramaters: student ID , the submissiosn Array
+function getCompletedAssignments(student,submissions){
+
+    let x = []
+    //iterte through submissions array 
+    for(let submitted of submissions ){
+        if(submitted.learner_id === student)
+            x.push(submitted.assignment_id)
+    }
+    return x
+
+}
+
+// here, we would process this data to achieve the desired result.
+function getLearnerData(course, ag, submissions) {
+    let results = [];
+   
+   let uniqueIDs = getUniqueLearnersIDs(submissions); //get the unique student IDs (Dictates how many objects we weill ultimatley need to return )
+
+   console.log("Our Unique Ids:")
+   console.log(uniqueIDs);
+
+   //create a reutrn object for each student 
+   for(let uniqueID of uniqueIDs){
+        let obj = {}
+        obj['id'] = uniqueID
+        results.push(obj)
+   }
+
+
+   //can probably put this in above loop for time complexity or somethign like that 
+   results = results.map( (student)=>
+    {   
+        let completedWork = getCompletedAssignments(student.id,submissions); //get me an array of the assigments that student complteted
+        //add a new property where the key is the assigment number: and the value is what they scored
+        for(let work of completedWork){
+            student[work] = getAssignmentScore(student.id,work,submissions)
+        }
+        return student //return the modifed object back into the array
+    }
+
+
+
+);
+
+   console.log(`\n Ultimate Output: `)
+   console.log(results)
+
+
+
+   
+}
+
+
+
+
+
+
+
+
 
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 
-console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
